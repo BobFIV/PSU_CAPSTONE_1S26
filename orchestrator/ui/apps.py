216b8ffa65b9f4b3d0e0fd974fc1b5afc69cd21a
aks7525 <aks7525@psu.edu>
2026-02-18 19:51:41 -0500

    inital code changes (still need to be tested) for Orchestrator to startup by also trying to register to the IN-CSE when launching locally

--- orchestrator/ui/apps.py
+++ orchestrator/ui/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
-
+import os
 
 class UiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ui"
+
+    def ready(self):
+        #prevent double run in dev server
+        if os.environ.get('RUN_MAIN') != True:
+            return
+        
+        from .services import create_starter_AE
+        from . import services
+
+        try:
+            status = create_starter_AE()
+            services.registration_status = "Successful Startup" #this shows that an AE was created
+            print("IN-CSE response:", status)
+        except Exception as e:
+            print("Error registering container:", e)
+
+
---
+++ orchestrator/ui/services.py
@@ -0,0 +1,38 @@
+import requests
+
+registration_status = "Startup not executed yet."
+
+def create_starter_AE():
+
+    # The URL of the oneM2M CSE
+    cse_url = 'http://localhost:8080/~/cse-in' #might need to change this depending
+
+    # Set the oneM2M headers
+    headers = {
+        'Content-Type': 'application/json;ty=2',
+        'X-M2M-Origin': 'CMyApplication',
+        'X-M2M-RI': '12345',
+        'X-M2M-RVI': '4'
+    }
+
+    # Define the oneM2M body for the AE
+    body = {
+        'm2m:ae': {
+            'rn': 'Corchestrator',
+            'api': 'Nmy-application.example.com',
+            'rr': True,
+            'srv': ['4']
+        }
+    }
+
+    # Send the request to the oneM2M CSE
+    response = requests.post(cse_url, headers=headers, json=body)
+
+    # Check the response status code
+    if response.status_code == 201:
+        print(f'AE registration successful: {response.status_code} / {response.headers["X-M2M-RSC"]}')
+        print(response.json())
+    else:
+        print(f'AE registration failed: {response.status_code} / {response.headers["X-M2M-RSC"]} {response.json()}')
+
+    return response.status_code
